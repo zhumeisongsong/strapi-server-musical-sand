eb902dd435e20ae0501cebc910c8af8eef98e057
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2021-11-22 20:15:08 +0900

    ✨

--- config/database.js
+++ config/database.js
@@ -1,14 +1,25 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,
+        pool: {
+          min: 0,
+          max: 15,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000,
+        },
       },
     },
   },
